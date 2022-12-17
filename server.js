ee1189bddd5694e4f69083f26ec5a85bfb41c20f
Vardaan <vardaanagrawal517@gmail.com>
2022-12-17 20:28:09 +0530

    bug fixed

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use("/", Routes);
 if (process.env.NODE_ENV == "production") {
   const path = require("path");
 
-  app.get("/", (req, res) => {
+  app.get("*", (req, res) => {
     app.use(express.static(path.resolve(__dirname, "frontend", "build")));
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
